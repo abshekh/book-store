44e24b4dae7833b56cbffdea8972e95d5bb31b5e
Abhishek Singh <abhishek17513@gmail.com>
2023-06-13 20:04:26 +0530

    use haskell flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,17 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.haskell-flake.url = "github:srid/haskell-flake";
   outputs =
     inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
       perSystem = { system, pkgs, devShells, ... }:
-        let
-          overlay = final: prev: {
-            book-store = final.callCabal2nix "book-store" ./. { };
-          };
-          myHaskellPackages = pkgs.haskell.packages.ghc927.extend overlay;
-        in
         {
-          packages.default = myHaskellPackages.book-store;
-          devShells = {
-            default = myHaskellPackages.shellFor {
-              packages = p: [
-                p.book-store
-              ];
-              buildInputs = with myHaskellPackages; [
-                cabal-install
-                ghcid
-                haskell-language-server
-                hindent
-              ];
-            };
-          };
+          haskellProjects.default = { };
         };
     };
 }
